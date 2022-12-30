aad734297f0e2c553a56ca12daed12ef5e664db2
FevenSeyfu <fevenseyfu00@gmail.com>
2022-12-30 21:48:21 +0300

    Added event to cancel button

---
+++ mobile-menu.js
@@ -0,0 +1,43 @@
+const hamburgerBtn = document.querySelector('.hamburger-icon');
+const cancelBtn = document.querySelector('.cancel-btn');
+const mainBody = document.querySelector('.main-body');
+const mobileScreen = window.matchMedia('screen and (max-width:768px)');
+const mobileMenu = document.querySelector('#mobile-menu');
+const mobileMenuContent = document.querySelectorAll('.nav-items .mobile');
+const menuElements = Array.from(mobileMenuContent);
+
+function reverseEvent() {
+  mainBody.style.display = 'block';
+  mobileMenu.style.display = 'none';
+}
+
+function displayMobileMenu() {
+  if (mobileScreen.matches) {
+    mainBody.style.display = 'none';
+    mobileMenu.style.display = 'block';
+    mobileMenu.classList.add('mobile-menu-style');
+    for (let i = 0; i < menuElements.length; i += 1) {
+      menuElements[i].style.display = 'block';
+    }
+  }
+  window.addEventListener('resize', reverseEvent);
+}
+
+function chooseSection(e) {
+  reverseEvent();
+  const sections = e.target.classList;
+  sections.forEach((section) => {
+    if (section === 'portfolio') {
+      window.location.href = '#portfolio';
+    } else if (section === 'about') {
+      window.location.href = '#about';
+    } else if (section === 'contact') {
+      window.location.href = '#contact';
+    }
+  });
+}
+hamburgerBtn.addEventListener('click', displayMobileMenu);
+cancelBtn.addEventListener('click', reverseEvent);
+for (let i = 0; i < menuElements.length; i += 1) {
+  menuElements[i].addEventListener('click', chooseSection);
+}
